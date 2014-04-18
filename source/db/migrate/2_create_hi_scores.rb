e14600723fa43210a142ff2bb6cd915f4bb4e711
Geoff Pavey <geoff.pavey@gmail.com>
2014-04-18 14:14:02 -0700

    fixed score"

--- source/db/migrate/2_create_hi_scores.rb
+++ source/db/migrate/2_create_hi_scores.rb
@@ -3,7 +3,7 @@ require_relative '../config'
 class CreateHiScores < ActiveRecord::Migration
 	def change
 		create_table :scores do |t|
-			t.integer :scores
+			t.integer :score
 			t.string :name
 		end
 	end
