235cea6d78df4d8c53afe0e01a3c8821de458ee3
Maggy Feldman <Mahkt6@gmail.com>
2014-04-18 15:21:35 -0700

    added high scores table to view

---
+++ source/View/images.rb
@@ -0,0 +1,276 @@
+module HangedMen
+	def self.empty_noose
+		print "\n"
+    print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint[" ____|_____________ \n","#663300", :bold]
+    print  Paint["|||||_||||||||||||||\n","#663300", :bold]
+    print  Paint["||||||||||||||||||||\n","#663300", :bold,:underline]
+	end
+
+	def self.head_only
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["         "             ,"00CC00", :bold]
+    print  Paint[" "										 ,"00CC00", :bold]
+    print  Paint["    \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint[" "									 	 ,"00CC00", :bold]
+		print	 Paint["      \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["          "            ,"#0066FF", :bold]
+    print  Paint["    \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        "              ,"0066FF", :bold]
+    print  Paint["    \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+
+	def self.head_and_torso
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["         "             ,"00CC00", :bold]
+    print  Paint["|"										 ,"00CC00", :bold]
+    print  Paint["    \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint[" "									 	 ,"00CC00", :bold]
+		print	 Paint[" |    \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["          "            ,"#0066FF", :bold]
+    print  Paint["    \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        "              ,"0066FF", :bold]
+    print  Paint["    \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+
+	def self.head_and_one_arm
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["        /"             ,"00CC00", :bold]
+    print  Paint["|"										 ,"00CC00", :bold]
+    print  Paint["    \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint["/"									 	 ,"00CC00", :bold]
+		print	 Paint[" |    \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["          "            ,"#0066FF", :bold]
+    print  Paint["    \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        "              ,"0066FF", :bold]
+    print  Paint["    \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+	def self.head_and_arms
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["        /"             ,"00CC00", :bold]
+    print  Paint["|"										 ,"00CC00", :bold]
+    print  Paint["\\  \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint["/"									 	 ,"00CC00", :bold]
+		print	 Paint[" | \\ \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["          "            ,"#0066FF",:bold]
+    print  Paint["    \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        "              ,"0066FF", :bold]
+    print  Paint["    \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+
+	def self.torso_and_leg
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["        /"             ,"00CC00", :bold]
+    print  Paint["|"										 ,"00CC00", :bold]
+    print  Paint["\\  \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint["/"									 	 ,"00CC00", :bold]
+		print	 Paint[" | \\ \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        / "            ,"#0066FF", :bold]
+    print  Paint["    \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["       /"              ,"0066FF", :bold]
+    print  Paint["      \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+
+	def self.dead_guy
+		print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |" 							 ,"#663300", :bold]
+    print  Paint["         @    \n"      ,"#FF9999", :bold]
+    print  Paint["     |"                ,"#663300", :bold]
+    print  Paint["        /"             ,"00CC00", :bold]
+    print  Paint["|"										 ,"00CC00", :bold]
+    print  Paint["\\  \n"                ,"00CC00", :bold]
+		print  Paint["     |       "				 ,"#663300", :bold]
+		print  Paint["/"									 	 ,"00CC00", :bold]
+		print	 Paint[" | \\ \n"              ,"00CC00", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["        / "            ,"#0066FF", :bold]
+    print  Paint["\\  \n"                ,"0066FF", :bold]
+    print  Paint["     |"								 ,"#663300", :bold]
+    print  Paint["       /"              ,"0066FF", :bold]
+    print  Paint["   \\ \n"              ,"0066FF", :bold]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold, :underline]
+	end
+end
+
+module Messages
+
+	def self.welcome
+print Paint["  _    _      _                            _
+ | |  | |    | |                          | |
+ | |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___
+ | |/\\| |/ _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\
+ \\  /\\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+  \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/
+
+
+ ____________  _____   _   _
+ |  _  \\ ___ \\/  __ \\ | | | |
+ | | | | |_/ /| /  \\/ | |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+ | | | | ___ \\| |     |  _  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
+ | |/ /| |_/ /| \\__/\\ | | | | (_| | | | | (_| | | | | | | (_| | | | |
+ |___/ \\____/  \\____/ \\_| |_/\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                                          __/ |
+                                         |___/  \n","#3D0000", :bold, :bright]
+		print Paint[
+"    ___              _          _   _           _
+   / __|_ _ ___ __ _| |_ ___ __| | | |__ _  _  (_)
+  | (__| '_/ -_) _` |  _/ -_) _` | | '_ \\ || |  _
+   \\___|_| \\___\\__,_|\\__\\___\\__,_| |_.__/\\_, | (_)
+                                         |__/ \n",:white, :bold, :bright]
+    print Paint[
+"      __  __ _ _      _
+  ___|  \\/  (_) |_ __| |_
+ |___| |\\/| | |  _/ _| ' \\
+     |_|  |_|_|\\__\\__|_||_|
+                           \n", :white, :bold, :bright]
+    print Paint[
+"      _  ___
+  ___| |/ (_)
+ |___| ' <| |
+     |_|\\_\\_|
+             \n", :white, :bold, :bright]
+    print Paint[
+"      __  __
+  ___|  \\/  |__ _ __ _ __ _ _  _
+ |___| |\\/| / _` / _` / _` | || |
+     |_|  |_\\__,_\]__, \\__, |\\_, |
+                 |___/|___/ |__/ \n", :white, :bold, :bright]
+    print Paint[
+"       ___          __  __
+  ___ / __|___ ___ / _|/ _|
+ |___| (_ / -_) _ \\  _|  _|
+      \\___\\___\\___/_| |_|
+                           \n", :white, :bold, :bright]
+    sleep(3)
+	end
+end
+
+module Screens
+		def self.menu_screen
+		print "\n" * 3
+		print Paint[
+" ____________  _____   _   _
+ |  _  \\ ___ \\/  __ \\ | | | |
+ | | | | |_/ /| /  \\/ | |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+ | | | | ___ \\| |     |  _  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
+ | |/ /| |_/ /| \\__/\\ | | | | (_| | | | | (_| | | | | | | (_| | | | |
+ |___/ \\____/  \\____/ \\_| |_/\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                                          __/ |
+                                         |___/  \n","#3D0000", :bold, :bright]
+    print "\n"
+		print  Paint["      _________     \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |         |    \n","#663300", :bold]
+    print  Paint["     |        (_)     ","#663300", :bold] + Paint["                 [1] New Game\n", :white, :bold, :bright]
+    print  Paint["     |              \n","#663300", :bold] 
+		print  Paint["     |                ","#663300", :bold] + Paint["                 [2] High Scores\n", :white, :bold, :bright]
+    print  Paint["     |              \n","#663300", :bold]
+    print  Paint["     |                ","#663300", :bold] + Paint["                 [3] Exit\n", :white, :bold, :bright]
+    print  Paint["     |              \n","#663300", :bold]
+		print  Paint[" ____|_____________ \n","#663300", :bold]
+		print  Paint["|||||_||||||||||||||\n","#663300", :bold]
+		print  Paint["||||||||||||||||||||\n","#663300", :bold,:underline]
+    print "\n" * 3
+    print Paint["          ---------------------------------------------\n", :white]
+    print Paint["           Enter the number of your selection to begin\n", :white, :blink, :bold]
+    print Paint["          ---------------------------------------------\n", :white]
+    print "\n" * 3
+	end
+
+		def self.highscores_screen
+		print "\n" * 3
+		print Paint[
+"  _    _ _       _        _____                         
+ | |  | (_)     | |      / ____|                        
+ | |__| |_  __ _| |__   | (___   ___ ___  _ __ ___  ___ 
+ |  __  | |/ _` | '_ \\   \\___ \\ / __/ _ \\| '__/ _ \\/ __|
+ | |  | | | (_| | | | |  ____) | (_| (_) | | |  __/\\__ \\
+ |_|  |_|_|\\__, |_| |_| |_____/ \\___\\___/|_|  \\___||___/
+            __/ |                                       
+           |___/                                       \n", "gold", :bold, :bright]
+    print "\n" * 3
+    print Paint[" _________________________________________________________\n","gold", :bold, :bright]
+    print Paint["|   Ranking   |          User             |     Score     |", "gold", :bold, :bright, :underline]
+		end
+end
+
+
